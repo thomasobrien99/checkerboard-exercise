f28ff8ef58e50eeb9150195fed82375ecf0aaa5d
Thomas O'Brien <thomasobrien99@gmail.com>
2016-02-29 20:14:40 -0800

    add a flashing branch

--- script.js
+++ script.js
@@ -3,9 +3,29 @@ var body = document.getElementsByTagName('body')[1];
 
 var counter = 0;
 
+setInterval(randomizeBG, 1000);
+
+function randomizeBG(){
+  for (var i = 0; i < 9; i++)
+  {
+  	for (var j = 0; j < 9; j++)
+  	{
+  		var randomRed = Math.floor(Math.random()*255);
+		  var randomGreen = Math.floor(Math.random()*255);
+		  var randomBlue = Math.floor(Math.random()*255);
+  		
+  		var currentCell = document.getElementsByClassName('row'+i+' column'+j)[0];
+
+  		currentCell.style.backgroundColor = "rgb("+randomRed+","+randomGreen+","+randomBlue+")";
+
+  	}
+  }
+
+}
 for(var i = 0; i < 9; i++)
 {
 	var row = document.createElement('div');
+	row.classList.add("row"+i);
 	for(var j = 0; j < 9; j++)
 	{
 		var randomRed = Math.floor(Math.random()*255);
@@ -13,6 +33,8 @@ for(var i = 0; i < 9; i++)
 		var randomBlue = Math.floor(Math.random()*255);
 
 		var column = document.createElement('div');
+		column.classList.add("row"+i);
+		column.classList.add("column"+j);
 		column.style.width = "11.1%";
 		column.style.backgroundColor = "rgb("+randomRed+","+randomGreen+","+randomBlue+")";
 		column.style.float = "left";
