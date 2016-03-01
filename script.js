1e162223f329600b6893375f7f85ef202d894ad9
Thomas O'Brien <thomasobrien99@gmail.com>
2016-02-29 18:52:04 -0800

    add a checkerboard variation with random colors

--- script.js
+++ script.js
@@ -8,17 +8,15 @@ for(var i = 0; i < 9; i++)
 	var row = document.createElement('div');
 	for(var j = 0; j < 9; j++)
 	{
+		var randomRed = Math.floor(Math.random()*255);
+		var randomGreen = Math.floor(Math.random()*255);
+		var randomBlue = Math.floor(Math.random()*255);
+
 		var column = document.createElement('div');
 		column.style.width = "11.1%";
-		column.style.backgroundColor = "red";
+		column.style.backgroundColor = "rgb("+randomRed+","+randomGreen+","+randomBlue+")";
 		column.style.float = "left";
 		column.style.paddingBottom= "11.1%";
-
-
-		if(counter % 2 === 0)
-		{
-			column.style.backgroundColor = "black";
-		}
 
 		row.appendChild(column);
 		counter++;
