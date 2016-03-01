f77e3c9b4894e5ffdfd21b3868a1bf7101d2ce84
Thomas O'Brien <thomasobrien99@gmail.com>
2016-02-29 18:43:39 -0800

    recreate checkerboard using only js

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 // Your JS goes here
+var body = document.getElementsByTagName('body')[1];
+
+var counter = 0;
+
+for(var i = 0; i < 9; i++)
+{
+	var row = document.createElement('div');
+	for(var j = 0; j < 9; j++)
+	{
+		var column = document.createElement('div');
+		column.style.width = "11.1%";
+		column.style.backgroundColor = "red";
+		column.style.float = "left";
+		column.style.paddingBottom= "11.1%";
+
+
+		if(counter % 2 === 0)
+		{
+			column.style.backgroundColor = "black";
+		}
+
+		row.appendChild(column);
+		counter++;
+	}
+  document.body.appendChild(row);
+}
