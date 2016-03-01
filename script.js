d453f872e75861e18ab6d28edff55b04994f0df8
Thomas O'Brien <thomasobrien99@gmail.com>
2016-02-29 19:13:54 -0800

    add a checkerboard variation with gradient

--- script.js
+++ script.js
@@ -3,18 +3,27 @@ var body = document.getElementsByTagName('body')[1];
 
 var counter = 0;
 
+var randomRedBase = Math.floor(Math.random()*50);
+var randomGreenBase = Math.floor(Math.random()*50);
+var randomBlueBase = Math.floor(Math.random()*50);
+
+var randomRed = randomRedBase;
+var randomGreen = randomGreenBase;
+var randomBlue = randomBlueBase;
+
 for(var i = 0; i < 9; i++)
 {
 	var row = document.createElement('div');
 	for(var j = 0; j < 9; j++)
 	{
-		var randomRed = Math.floor(Math.random()*255);
-		var randomGreen = Math.floor(Math.random()*255);
-		var randomBlue = Math.floor(Math.random()*255);
-
+		randomRed = Math.floor(randomRed*1.03);
+    randomGreen = Math.floor(randomGreen*1.03);
+    randomBlue = Math.floor(randomBlue*1.03);
+		
 		var column = document.createElement('div');
 		column.style.width = "11.1%";
 		column.style.backgroundColor = "rgb("+randomRed+","+randomGreen+","+randomBlue+")";
+
 		column.style.float = "left";
 		column.style.paddingBottom= "11.1%";
 
